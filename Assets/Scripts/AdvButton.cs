68e8c59ee5194bfec2fb83d7e50758e8d1b45490
GumpFlash <gump.flash@gmail.com>
2018-09-21 12:48:05 -0300

    ep5

---
+++ Assets/Scripts/AdvButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AdvButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+
+    public bool hold;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        hold = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        hold = false;
+    }
+}
---
+++ Assets/Scripts/Peek.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Peek : MonoBehaviour {
+
+    public Camera cam;
+    public float time;
+    private int peek = 0;
+    
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (peek >= 0)
+                peek = -1;
+            else
+                peek = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (peek <= 0)
+                peek = 1;
+            else
+                peek = 0;
+        }
+
+        if(peek == -1)
+        {
+            cam.transform.eulerAngles = Vector3.LerpUnclamped(cam.transform.eulerAngles, new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, 25), time * Time.deltaTime);
+            cam.transform.localPosition = Vector3.LerpUnclamped(cam.transform.localPosition, new Vector3(-0.5f, cam.transform.localPosition.y, cam.transform.localPosition.z), time * Time.deltaTime);
+        }else if (peek == 1)
+        {
+            cam.transform.eulerAngles = Vector3.LerpUnclamped(cam.transform.eulerAngles, new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, -25), time * Time.deltaTime);
+            cam.transform.localPosition = Vector3.LerpUnclamped(cam.transform.localPosition, new Vector3(0.5f, cam.transform.localPosition.y, cam.transform.localPosition.z), time * Time.deltaTime);
+        }
+        else
+        {
+            cam.transform.eulerAngles = Vector3.LerpUnclamped(cam.transform.eulerAngles, new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, 0), time * Time.deltaTime);
+            cam.transform.localPosition = Vector3.LerpUnclamped(cam.transform.localPosition, new Vector3(0, cam.transform.localPosition.y, cam.transform.localPosition.z), time * Time.deltaTime);
+        }
+    }
+}
